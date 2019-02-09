4792c664c265192cfb72db93c95ad4e0370473a4
Tanner Culberson <tannerculberson@Tanners-MacBook-Pro.local>
2019-02-09 07:10:29 -0500

    finished treasure hunt portion

--- src/Main.java
+++ src/Main.java
@@ -8,29 +8,62 @@ public class Main {
 		System.out.println("input:\n" + testStr);
 		System.out.println("\noutput:\n" + toCamelCase(testStr));
 
-		/*
-		 * Random rand = new Random(); int howManyCaves = rand.nextInt(9) + 1;
-		 * int howManyFlashlights = rand.nextInt(3) + 1; int[] caves = new
-		 * int[howManyCaves]; int[] flashlights = new int[howManyFlashlights];
-		 * for (int i = 0; i < caves.length; i++) caves[i] = rand.nextInt(8) +
-		 * 1; for (int i = 0; i < flashlights.length; i++) caves[i] =
-		 * rand.nextInt(999999) + 1; System.out.println("\n\nCaves: " +
-		 * Arrays.toString(caves)); System.out.println("Flashlights: " +
-		 * Arrays.toString(flashlights));
-		 * 
-		 * System.out.println("\nI have " + howManyCaves +
-		 * "caves to search and " + howManyFlashlights + "to do it with");
-		 * System.out.println("I have found " + treasureHunt(caves, flashlights)
-		 * + " treasure chests!");
-		 */
+		Random rand = new Random();
+		int howManyCaves = rand.nextInt(9) + 1;
+		int howManyFlashlights = rand.nextInt(3) + 1;
+		int[] caves = new int[howManyCaves];
+		int[] flashlights = new int[howManyFlashlights];
+		
+		for (int i = 0; i < caves.length; i++)
+			caves[i] = rand.nextInt(8) + 1;
+		
+		for (int i = 0; i < flashlights.length; i++)
+			flashlights[i] = rand.nextInt(999999) + 1;
+		
+		System.out.println("\n\nCaves: " + Arrays.toString(caves));
+		System.out.println("Flashlights: " + Arrays.toString(flashlights));
+
+		System.out.println("\nI have " + howManyCaves + " caves to search and " + howManyFlashlights + " to do it with");
+		System.out.println("I have found " + treasureHunt(caves, flashlights) + " treasure chests!");
+
 	}
 
-	// public static int treasureHunt(int[] caves, int[] flashlights) {
-	// return 0;
-	//
-	// }
-	// public static boolean searchCave(int caves, int flashlights) {
-	// }
+	public static int treasureHunt(int[] caves, int[] flashlights) {
+		int foundTreasure = 0;
+		
+		for(int i = 0; i < caves.length; i++) {
+			for(int j = 0; j < flashlights.length; j++) {
+				if(searchCave(caves[i], flashlights[j])) {
+					foundTreasure++;
+				}
+			}
+		}
+
+		return foundTreasure;
+	}
+
+	public static boolean searchCave(int caves, int flashlights) {
+		int digits = String.valueOf(flashlights).length();
+		if(digits > 1) {
+			flashlights = sumOfDigits(flashlights);
+		}
+		
+		if(caves == flashlights) {
+			return true;
+		}
+		
+		if(caves != flashlights && digits == 1) {
+			return false;
+		}	
+        
+		return searchCave(caves, flashlights);
+	}
+	
+	private static int sumOfDigits(int n) {  
+        if (n == 0) 
+            return 0; 
+        return (n % 10 + sumOfDigits(n / 10)); 
+    }
 
 	public static String toCamelCase(String str) {
 		str = str.trim().replaceAll("\\s+", " ");
@@ -52,7 +85,7 @@ public class Main {
 
 		String word = words[counter].toLowerCase();
 		camelCaseStr += word.substring(0, 1).toUpperCase() + word.substring(1, word.length());
-		
+
 		counter++;
 		return makeWordsCamelCase(words, camelCaseStr, counter);
 	}
